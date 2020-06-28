401a14f9773d6cd55a3cb48e5a7a658f704f3bcc
PulkitGargV1 <63607257+PulkitGargV1@users.noreply.github.com>
2020-06-28 22:46:24 +0100

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -9,10 +9,10 @@ dockerpath="gargpulkit/tourism_app"
 
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login &&\
-    docker image tag tourism_app $dockerpath
+
+docker docker login --username $1 --password $2 
+docker image tag tourism_app $dockerpath:latest
 
 # Push image to a docker repository
 
-docker image push $dockerpath 
-
+docker image push $dockerpath:latest
